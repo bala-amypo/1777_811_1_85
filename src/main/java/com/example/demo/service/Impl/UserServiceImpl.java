5c8df7b758e9bc4d81df400f6d1a1ffc78f548b6
bala-amypo <bala@amypo.in>
2025-12-19 02:23:31 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.demo.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.UserEntity;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository repo;
+
+    @Override
+    public UserEntity register(UserEntity user) {
+        return repo.save(user);
+    }
+
+    @Override
+    public UserEntity findByEmail(String email) {
+        return repo.findByEmail(email).orElse(null);
+    }
+}
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,26 +1,10 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.UserEntity;
-import com.example.demo.repository.UserRepository;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 
-@Service
-public class UserService {
+public interface UserService {
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder encoder;
+    UserEntity register(UserEntity user);
 
-    public UserService(UserRepository userRepository, PasswordEncoder encoder) {
-        this.userRepository = userRepository;
-        this.encoder = encoder;
-    }
-
-    public UserEntity register(UserEntity user) {
-        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
-            throw new RuntimeException("Email already in use");
-        }
-        user.setPassword(encoder.encode(user.getPassword()));
-        return userRepository.save(user);
-    }
+    UserEntity findByEmail(String email);
 }
