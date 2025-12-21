64102b51b4c06de7e7fca723bd62cbe68e3e9a83
bala-amypo <bala@amypo.in>
2025-12-21 05:39:20 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/PropertyController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.entity.Property;
+import com.example.demo.service.PropertyService;
+
+@RestController
+@RequestMapping("/properties")
+public class PropertyController {
+
+    private final PropertyService propertyService;
+
+    public PropertyController(PropertyService propertyService) {
+        this.propertyService = propertyService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Property> addProperty(@RequestBody Property property) {
+        Property savedProperty = propertyService.addProperty(property);
+        return new ResponseEntity<>(savedProperty, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Property>> getAllProperties() {
+        List<Property> properties = propertyService.getAllProperties();
+        return ResponseEntity.ok(properties);
+    }
+}
