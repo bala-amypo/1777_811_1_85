eeab9e07986e9df954688d684881843b850f73d6
bala-amypo <bala@amypo.in>
2025-12-21 06:06:09 +0000

    User commit

--- src/main/java/com/example/demo/controller/PropertyController.java
+++ src/main/java/com/example/demo/controller/PropertyController.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.example.demo.entity.Property;
+import com.example.demo.entity.PropertyEntity;
 import com.example.demo.service.PropertyService;
 
 @RestController
