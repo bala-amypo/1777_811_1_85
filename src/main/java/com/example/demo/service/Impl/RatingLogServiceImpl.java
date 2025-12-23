84ee17b3cc1dafe83faa37cc1fca7d6e44aa3fdd
bala-amypo <bala@amypo.in>
2025-12-23 06:54:53 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/RatingLogServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/RatingLogServiceImpl.java
@@ -6,6 +6,7 @@ import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.PropertyRepository;
 import com.example.demo.repository.RatingLogRepository;
 import com.example.demo.service.RatingLogService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,31 +14,31 @@ import java.util.List;
 @Service
 public class RatingLogServiceImpl implements RatingLogService {
 
-    private final RatingLogRepository logRepo;
-    private final PropertyRepository propertyRepo;
+    @Autowired
+    private RatingLogRepository ratingLogRepository;
 
-    public RatingLogServiceImpl(RatingLogRepository logRepo,
-                                PropertyRepository propertyRepo) {
-        this.logRepo = logRepo;
-        this.propertyRepo = propertyRepo;
-    }
+    @Autowired
+    private PropertyRepository propertyRepository;
 
     @Override
     public RatingLogEntity addLog(Long propertyId, String message) {
 
-        PropertyEntity property = propertyRepo.findById(propertyId)
+        PropertyEntity property = propertyRepository.findById(propertyId)
                 .orElseThrow(() -> new ResourceNotFoundException("Property not found"));
 
-        RatingLogEntity log = new RatingLogEntity(property, message);
-        return logRepo.save(log);
+        RatingLogEntity log = new RatingLogEntity();
+        log.setProperty(property);
+        log.setMessage(message);
+
+        return ratingLogRepository.save(log);
     }
 
     @Override
     public List<RatingLogEntity> getLogsByProperty(Long propertyId) {
 
-        PropertyEntity property = propertyRepo.findById(propertyId)
+        PropertyEntity property = propertyRepository.findById(propertyId)
                 .orElseThrow(() -> new ResourceNotFoundException("Property not found"));
 
-        return logRepo.findByProperty(property);
+        return ratingLogRepository.findByProperty(property);
     }
 }
