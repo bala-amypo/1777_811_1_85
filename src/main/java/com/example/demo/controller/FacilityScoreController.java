b1a5e81dcf879328e9776e6a3861ae862c9c2e47
bala-amypo <bala@amypo.in>
2025-12-23 03:39:46 +0000

    User commit

--- src/main/java/com/example/demo/controller/FacilityScoreController.java
+++ src/main/java/com/example/demo/controller/FacilityScoreController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.entity.FacilityScoreEntity;
 import com.example.demo.service.FacilityScoreService;
 import org.springframework.web.bind.annotation.*;
