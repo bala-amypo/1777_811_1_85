d07fd608d577a21796a2e3d10f1da087ac9b647d
bala-amypo <bala@amypo.in>
2025-12-23 06:57:59 +0000

    User commit

--- src/main/java/com/example/demo/controller/RatingController.java
+++ src/main/java/com/example/demo/controller/RatingController.java
@@ -1,12 +1,16 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.RatingEntity;
+import com.example.demo.service.RatingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping("/ratings")
 public class RatingController {
 
-    private final RatingService ratingService;
-
-    public RatingController(RatingService ratingService) {
-        this.ratingService = ratingService;
-    }
+    @Autowired
+    private RatingService ratingService;
 
     @PostMapping("/generate/{propertyId}")
     public RatingEntity generateRating(@PathVariable Long propertyId) {
