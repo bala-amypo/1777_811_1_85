e89b9f8201ef213d5237635fe792635a92c30ad8
bala-amypo <bala@amypo.in>
2025-12-26 08:53:19 +0000

    User commit

--- src/main/java/com/example/demo/entity/Property.java
+++ src/main/java/com/example/demo/entity/Property.java
@@ -1,113 +1,45 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Entity
-@Table(name = "properties")
 public class Property {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank
-    private String title;
+    private String name;
 
-    @NotBlank
-    private String address;
-
-    @NotBlank
-    private String city;
-
-    @NotNull
-    @Positive
-    private Double price;
-
-    @NotNull
-    @Positive
-    private Double areaSqFt;
-
-    @OneToOne(mappedBy = "property", cascade = CascadeType.ALL)
-    private FacilityScore facilityScore;
-
-    @OneToOne(mappedBy = "property", cascade = CascadeType.ALL)
-    private RatingResult ratingResult;
-
-    @OneToMany(mappedBy = "property", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<RatingLog> ratingLogs = new ArrayList<>();
-
+    // 🔴 mappedBy MUST MATCH User field name EXACTLY
     @ManyToMany(mappedBy = "assignedProperties")
     private Set<User> assignedUsers = new HashSet<>();
 
-    // ===== Helper =====
-    public void addRatingLog(RatingLog log) {
-        ratingLogs.add(log);
-        log.setProperty(this);
-    }
-
-    // ===== Getters & Setters =====
+    // ===== getters & setters =====
 
     public Long getId() {
         return id;
-    }
-
-    public String getTitle() {
-        return title;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAddress() {
-        return address;
+    public String getName() {
+        return name;
     }
  
-    public void setAddress(String address) {
-        this.address = address;
-    }
- 
-    public String getCity() {
-        return city;
-    }
- 
-    public void setCity(String city) {
-        this.city = city;
-    }
- 
-    public Double getPrice() {
-        return price;
-    }
- 
-    public void setPrice(Double price) {
-        this.price = price;
-    }
- 
-    public Double getAreaSqFt() {
-        return areaSqFt;
-    }
- 
-    public void setAreaSqFt(Double areaSqFt) {
-        this.areaSqFt = areaSqFt;
-    }
-
-    public List<RatingLog> getRatingLogs() {
-        return ratingLogs;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public Set<User> getAssignedUsers() {
         return assignedUsers;
+    }
+
+    public void setAssignedUsers(Set<User> assignedUsers) {
+        this.assignedUsers = assignedUsers;
     }
 }
